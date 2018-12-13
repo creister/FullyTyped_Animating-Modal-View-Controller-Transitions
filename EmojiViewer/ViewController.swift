c13786738132387af2c3a17589c0a69ec59331f6
Colin Reisterer <creisterer@projectdecibel.com>
2018-12-12 20:36:29 -0500

    Xcode new single file project

---
+++ EmojiViewer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EmojiViewer
+//
+//  Created by Colin Reisterer on 12/12/18.
+//  Copyright © 2018 FullyTyped. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
